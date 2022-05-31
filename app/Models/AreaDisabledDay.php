df931af791ced2fe5cfaff00162b67534de458fb
João Paulo Fontana Macedo <jpfontana12@icloud.com>
2022-05-31 10:10:01 -0300

    Added models

---
+++ app/Models/AreaDisabledDay.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AreaDisabledDay extends Model
+{
+    use HasFactory;
+
+    use HasFactory;
+
+    public $timestamps = false;
+    public $table = 'areadisableddays';
+}
