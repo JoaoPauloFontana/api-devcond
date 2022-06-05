dcf3f4e6d556f6033bfa208340878285c8fdc2ce
João Paulo Fontana Macedo <61567948+Shaiqna@users.noreply.github.com>
2022-06-05 14:46:44 -0300

    Added user login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\Unit;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -54,6 +56,55 @@ class AuthController extends Controller
 
         }else{
             $array['error'] = 'Não foi possível cadastrar o usuário!';
+
+            return $array;
+        }
+
+        return $array;
+    }
+
+    public function login(Request $req){
+        $array = ['error' => ''];
+
+        $validator = Validator::make($req->all(), [
+            'cpf' => 'required|digits:11',
+            'password' => 'required'
+        ]);
+
+        if (!$validator->fails()) {
+            $cpf = $req['cpf'];
+            $password = $req['password'];
+
+            if($password == '' || $cpf == ''){
+                $array['error'] = 'CPF ou senha são obrigatórios!';
+
+                return $array;
+            }
+
+            $token = auth()->attempt([
+                'cpf' => $req['cpf'],
+                'password' => $req['password']
+            ]);
+
+            if(!$token){
+                $array['error'] = 'CPF e/ou senha estão errados!';
+                return $array;
+            }
+
+            $array['token'] = $token;
+
+            $userLogged = auth()->user();
+            $array['user'] = $userLogged;
+
+            $properties = Unit::select(['id', 'name'])
+            ->where('id_owner', $userLogged['id'])
+            ->get();
+
+            $array['user']['properties'] = $properties;
+        }else{
+            $array['error'] = $validator->errors()->first();
+
+            return $array;
         }
 
         return $array;
