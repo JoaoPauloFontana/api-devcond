67d3a348132170e0a0305f1413834e72ebf3db3f
João Paulo Fontana Macedo <61567948+Shaiqna@users.noreply.github.com>
2022-06-15 20:45:50 -0300

    Added error message return

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -31,4 +31,20 @@ class RegisterRequest extends FormRequest
             'password_confirm' => 'required|same:password'
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'O nome é obrigatório!',
+            'email.required' => 'O e-mail é obrigatório!',
+            'email.email' => 'O e-mail informado não é válido!',
+            'email.unique' => 'O e-mail informado já está cadastrado!',
+            'cpf.required' => 'O CPF é obrigatório!',
+            'cpf.digits' => 'O CPF informado não é válido!',
+            'cpf.unique' => 'O CPF informado já está cadastrado!',
+            'password.required' => 'A senha é obrigatória!',
+            'password_confirm.required' => 'A confirmação de senha é obrigatória!',
+            'password_confirm.same' => 'A confirmação de senha não confere!'
+        ];
+    }
 }
