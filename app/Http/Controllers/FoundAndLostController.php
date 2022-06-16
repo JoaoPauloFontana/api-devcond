da96ac4c9fd6d6bddc8aac352f9cee4fa4363b54
João Paulo Fontana Macedo <61567948+Shaiqna@users.noreply.github.com>
2022-06-16 18:30:43 -0300

    Added method of getting all lost and found items

--- app/Http/Controllers/FoundAndLostController.php
+++ app/Http/Controllers/FoundAndLostController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\FoundAndLost;
 use Illuminate\Http\Request;
 
 class FoundAndLostController extends Controller
 {
-    //
+    public function getAll()
+    {
+        $array = ['error' => ''];
+
+        $lost = FoundAndLost::where('status', 'LOST')
+            ->orderBy('datecreated', 'DESC')
+            ->orderBy('id', 'DESC')
+            ->get();
+
+        $recovered = FoundAndLost::where('status', 'RECOVERED')
+            ->orderBy('datecreated', 'DESC')
+            ->orderBy('id', 'DESC')
+            ->get();
+
+        foreach($lost as $lostKey => $lostValue) {
+            $lost[$lostKey]['datecreated'] = date('d/m/Y', strtotime($lostValue->datecreated));
+            $lost[$lostKey]['photo'] = asset('storage/' . $lostValue->photo);
+        }
+
+        foreach($recovered as $recoveredKey => $recoveredValue) {
+            $recovered[$recoveredKey]['datecreated'] = date('d/m/Y', strtotime($recoveredValue->datecreated));
+            $recovered[$recoveredKey]['photo'] = asset('storage/' . $recoveredValue->photo);
+        }
+
+        $array['recovered'] = $recovered;
+        $array['lost'] = $lost;
+
+        return $array;
+    }
 }
