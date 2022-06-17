632b5e55aa99c1757a9d407aaec57242dd7c3a14
João Paulo Fontana Macedo <61567948+Shaiqna@users.noreply.github.com>
2022-06-16 21:58:06 -0300

    Added method of getting information from a property

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Unit;
+use App\Models\UnitPeople;
+use App\Models\UnitPet;
+use App\Models\UnitVehicle;
 use Illuminate\Http\Request;
 
 class UnitController extends Controller
 {
-    //
+    public function getInfo($id)
+    {
+        $array = ['error' => ''];
+
+        $unit = Unit::find($id);
+
+        if($unit){
+            $peoples = UnitPeople::where('id_unit', $id)->get();
+            $vehicles = UnitVehicle::where('id_unit', $id)->get();
+            $pets = UnitPet::where('id_unit', $id)->get();
+
+            foreach($peoples as $peopleKey => $people){
+                $peoples[$peopleKey]['birthdate'] = date('d/m/Y', strtotime($people->birthdate));
+            }
+
+            $array['peoples'] = $peoples;
+            $array['vehicles'] = $vehicles;
+            $array['pets'] = $pets;
+        }else{
+            $array['error'] = 'Propriedade inexistente!';
+
+            return $array;
+        }
+
+        return $array;
+    }
 }
